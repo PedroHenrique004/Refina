0dffb8595f2e2e4e806afc3a69ea94f6deb39fbc
Pedro Santos <pedrohenriqueph004@gmail.com>
2025-06-27 09:48:37 -0300

    Initial Commit

---
+++ Refina/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Refina
+//
+//  Created by Pedro Santos on 27/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
